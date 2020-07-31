e9e907fb10002eb0f220190f6ca5a75098a372fd
DarkLordDudeALT <42117154+DarkLordDudeALT@users.noreply.github.com>
2020-07-31 15:42:56 -0400

    Fixed math of vinegarSodaCalc.py. Added the Automate Flows I have recently created. They're not really diffable, but whatever.

--- Python Code/vinegarSodaCalc.py
+++ Python Code/vinegarSodaCalc.py
@@ -1,8 +1,12 @@
 import sys
+import math
 
 print()
 
 pressure = 0
+volume = 0
+CH3COOH_concentration = 0
+
 
 try:
     pressure = float(input("Enter the pressure that needs to be generated (in psi): "))
@@ -13,10 +17,6 @@ except Exception as exception:
 if pressure <= 0:
     sys.exit("Pressure must be greater than 0!")
 
-# Converts psi to atm
-pressure *= 0.06804596379
-volume = 0
-
 try:
     volume = float(input("Enter the volume of the container (in L): "))
 
@@ -25,19 +25,6 @@ except Exception as exception:
 
 if volume <= 0:
     sys.exit("Volume must be greater than 0!")
-
-# Finds the mass of the air in the container, assuming temperature is 21*C and the starting pressure is 1 atm. 
-mol_air = volume / 0.082057366080960 / 294.15
-# Finds the mass of CO2 needed to generate the extra pressure, assuming the starting pressure is 1 atm. 
-mol_CO2 = mol_air * pressure
-
-print()
-print(str(mol_CO2 * 84) + "g of NaHCO3 (baking soda) is needed")
-g_CH3COOH = mol_CO2 * 60
-print(str(g_CH3COOH) + "g of CH3COOH (acetic acid) is needed")
-print()
-
-CH3COOH_concentration = 0
 
 try:
     CH3COOH_concentration = float(input("Enter the vinegar's concentration of CH3COOH (acetic acid): "))
@@ -48,8 +35,18 @@ except Exception as exception:
 if CH3COOH_concentration <= 0 or CH3COOH_concentration >= 100:
     sys.exit("The concentration must be between 0 and 100 (exclusive)!")
 
-CH3COOH_concentration /= 100
-L_CH3COOH = g_CH3COOH * 0.00104938
+# Converts psi to atm, accounts for vapor pressure of water.
+pressure = pressure * 0.06804596379 - 0.0245
+CH3COOH_concentration_p = CH3COOH_concentration / 100
+
+
+# Finds the mass of the air in the container, assuming temperature is 21*C and the starting pressure is 1 atm. 
+mol_air = volume / 0.08205736608096 / 294.15
+# Finds the mass of CO2 needed to generate the extra pressure, assuming the starting pressure is 1 atm. 
+mol_CO2 = mol_air * pressure - mol_air
+# Finds the volume of vinegar needed, converts mol to g to L. Vinegar is CH3COOH_concentration% acetic acid by weight.
+L_vinegar = mol_CO2 * 60 * (0.1 / CH3COOH_concentration)
 
 print()
-print(str(L_CH3COOH / CH3COOH_concentration) + " L of vinegar is needed")
+print(str(mol_CO2 * 84) + "g of NaHCO3 (baking soda) is needed")
+print(str(L_vinegar) + " L of vinegar is needed")
